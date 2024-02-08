4b5f966b75b8934febe1eef28ba743f8b4a41ac4
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-02-08 04:03:00 -0300

    chore: add husky, lint-staged and commitlint

---
+++ .husky/install.mjs
@@ -0,0 +1,21 @@
+// Skip Husky install in production and CI
+if (isProduction() || isCI()) {
+  console.log("Skipping Husky installation in production or CI environment.");
+  process.exit(0);
+} else {
+  installHusky();
+}
+
+const installHusky = async () => {
+  try {
+    (await import("husky")).default();
+    console.log("Husky installed successfully");
+  } catch (error) {
+    console.error("Error installing Husky:", error);
+    process.exit(1);
+  }
+};
+
+const isProduction = () => process.env.NODE_ENV === "production";
+
+const isCI = () => process.env.CI === "true";
